d77bde8faeb4f3554bc2d5bc0821465fc76ca9c2
Irina <valaevai@yandex.ru>
2026-08-24 17:32:14 +0300

    Refresh hero asset versions

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createSiteShell } from './components/site-shell.js?v=20260824-28';
-import { createHomePage } from './components/home-sections.js?v=20260824-30';
+import { createHomePage } from './components/home-sections.js?v=20260824-31';
 import { createAboutPage, createArticleLayout, createContentHubPage, createFaqPage, createPricingPage, createPrivacyPage, createReleasesPage, createSearchPage, createSupportPage } from './components/content-pages.js?v=20260824-31';
 import { pageMeta } from './data/site.js?v=20260824-28';
 import { createProductDeviceMockup } from './components/product-device-mockup.js';
